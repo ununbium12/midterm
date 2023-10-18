1f18ec39795e66d68b98193c77eac221f0b26dff
정다훈 <ununbium@naver.com>
2023-10-18 13:26:45 +0900

    Add Commit 2
    두번째 커밋

---
+++ pages/About.js
@@ -0,0 +1,12 @@
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <code style={styles.code}>/pages/About.js 정다훈 201930325</code>
+    </>
+  )
+}
---
+++ pages/Contact.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <code style={styles.code}>/pages/Contact.js 정다훈 201930325</code>
+    </>
+  )
+}
